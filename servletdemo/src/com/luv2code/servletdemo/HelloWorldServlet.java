bd47dca2200457afda5ef4d36a00cffa80ea73a4
miguel <miguel@localhost>
2024-11-28 09:11:24 +0200

    Servlet Fundamentals

---
+++ servletdemo/src/com/luv2code/servletdemo/HelloWorldServlet.java
@@ -0,0 +1,54 @@
+package com.luv2code.servletdemo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Step 1: set the content type
+		response.setContentType("text/html");
+		
+		//Step 2: get the printwriter
+		PrintWriter out = response.getWriter();
+		
+		//Step 3: generate HTML content
+		out.println("<html><body>");
+		out.println("<h2>Hello World</h2>");
+		out.println("<hr>");
+		out.println("Time on the server is:" + new java.util.Date());
+		
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ servletdemo/src/com/luv2code/servletdemo/StudentServlet.java
@@ -0,0 +1,53 @@
+package com.luv2code.servletdemo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Step 1: set the content type
+		response.setContentType("text/html");
+				
+		//Step 2: get the printwriter
+		PrintWriter out = response.getWriter();
+				
+		//Step 3: generate HTML content
+		out.println("<html><body>");
+		
+		out.println("The studen is confirmed: " + request.getParameter("firstName")+" "+request.getParameter("lastName"));
+				
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
