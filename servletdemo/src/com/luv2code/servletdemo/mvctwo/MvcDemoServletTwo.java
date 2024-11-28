f5f69a07f4aac06b180ce540667f07c65739f633
miguel <miguel@localhost>
2024-11-28 13:37:05 +0200

    MVC - Build an MVC App with Servlets and JSP

---
+++ servletdemo/src/com/luv2code/servletdemo/mvctwo/MvcDemoServletTwo.java
@@ -0,0 +1,51 @@
+package com.luv2code.servletdemo.mvctwo;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Servlet implementation class MvcDemoServletTwo
+ */
+@WebServlet("/MvcDemoServletTwo")
+public class MvcDemoServletTwo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MvcDemoServletTwo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//step 1:get the student data from helper class (model)
+		List<Student> students = StudentDataUtil.getStudents();
+		//step 2: add students to request object
+		request.setAttribute("student_list", students);
+		//step 3: get request dispatcher
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view_students_two.jsp");
+		
+		//Step 4: forward the request to JSP
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
